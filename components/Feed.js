c0505012382a52e8182d857e92ba5fa9ca073c54
Rafael Soriano Ramírez <rafa.soriano.ramirez@gmail.com>
2023-02-01 13:45:22 -0600

    [Add]: Popularte fake data using minifaler and map through inside story component

---
+++ components/Feed.js
@@ -0,0 +1,19 @@
+import Stories from "./Stories";
+
+export default function Feed() {
+  return (
+    <main>
+      <section>
+        <Stories />
+
+        {/**Posts */}
+      </section>
+
+      <section>
+        {/**Mini profile */}
+
+        {/**Suggections */}
+      </section>
+    </main>
+  );
+}
--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import React from "react";
 import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
 
