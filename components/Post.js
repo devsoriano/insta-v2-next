f5b0d9c8ebb081e500c76ff5cdda44f87983024e
Rafael Soriano Ramírez <rafa.soriano.ramirez@gmail.com>
2023-02-01 15:15:06 -0600

    [Add]: Caption and input box of the post comment

--- components/Post.js
+++ components/Post.js
@@ -3,6 +3,7 @@ import {
   HeartIcon,
   ChatIcon,
   BookmarkIcon,
+  EmojiHappyIcon,
 } from "@heroicons/react/outline";
 
 export default function Post({ id, username, userImg, img, caption }) {
@@ -27,6 +28,21 @@ export default function Post({ id, username, userImg, img, caption }) {
         </div>
         <BookmarkIcon className="btn" />
       </div>
+
+      <p className="p-5 truncate">
+        <span className="font-bold">{username}</span>
+        {caption}
+      </p>
+
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="h-7" />
+        <input
+          className="border-none flex-1 focus:ring-0"
+          type="text"
+          placeholder="Enter your comment..."
+        />
+        <button className="text-blue-400 font-bold">Post</button>
+      </form>
     </div>
   );
 }
