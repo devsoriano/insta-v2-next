99c5f47f807152fd0b5dc3842dfc86a5bc655885
Rafael Soriano Ramírez <rafa.soriano.ramirez@gmail.com>
2023-02-01 11:42:56 -0600

    [Add] Sticky header

--- components/Header.js
+++ components/Header.js
@@ -10,36 +10,38 @@ const LOGO_INSTA_SM =
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-      {/** Left */}
-      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-        <Image src={LOGO_INSTA_LG} layout="fill" className="object-contain" />
-      </div>
-      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-        <Image src={LOGO_INSTA_SM} layout="fill" className="object-contain" />
-      </div>
-
-      {/** Middle */}
-      <div className="relative mt-1">
-        <div className="absolute top-2 left-2">
-          <SearchIcon className="h-5 text-gray-500" />
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        {/** Left */}
+        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+          <Image src={LOGO_INSTA_LG} layout="fill" className="object-contain" />
         </div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
-        />
-      </div>
+        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+          <Image src={LOGO_INSTA_SM} layout="fill" className="object-contain" />
+        </div>
 
-      {/** Right */}
-      <div className="flex space-x-4 items-center">
-        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <img
-          src="https://media.licdn.com/dms/image/D5603AQH0nCUmbkvglw/profile-displayphoto-shrink_400_400/0/1674767572195?e=1680739200&v=beta&t=DInTnPEDIGo1NUF-ri_Q08gWXgz8fRfCrHPSJBByO_Y"
-          alt="user-img"
-          className="h-10 rounded-full cursor-pointer"
-        />
+        {/** Middle */}
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <SearchIcon className="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+
+        {/** Right */}
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <img
+            src="https://media.licdn.com/dms/image/D5603AQH0nCUmbkvglw/profile-displayphoto-shrink_400_400/0/1674767572195?e=1680739200&v=beta&t=DInTnPEDIGo1NUF-ri_Q08gWXgz8fRfCrHPSJBByO_Y"
+            alt="user-img"
+            className="h-10 rounded-full cursor-pointer"
+          />
+        </div>
       </div>
     </div>
   );
