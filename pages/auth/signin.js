1ec85dd4abe744a3b139549c94d8d2a490c514a8
Rafael Soriano Ramírez <rafa.soriano.ramirez@gmail.com>
2023-02-02 03:50:38 -0600

    [Add]: Complete signin page

---
+++ pages/auth/signin.js
@@ -0,0 +1,45 @@
+import { getProviders, signIn } from "next-auth/react";
+import Header from "../../components/Header";
+
+export default function signn({ providers }) {
+  console.log("providers---", providers);
+  return (
+    <>
+      <Header />
+      <div className="flex justify-center space-x-7 mt-20">
+        <img
+          className="hidden object-cover rotate-6 md:inline-flex md:w-48"
+          src="https://superviral.com.au/wp-content/uploads/2021/08/instagix-banner-graphic.png"
+          alt=""
+        />
+        <div className="">
+          {Object.values(providers).map(({ name, id }) => (
+            <div className="flex flex-col items-center" key={name}>
+              <img
+                className="w-32 object-cover"
+                src="https://socodigital.com/wp-content/uploads/2021/03/Instagram.png"
+                alt="insta_logo"
+              />
+              <p className="text-sm italic my-10 text-center">
+                This app is created for learning purposes
+              </p>
+              <button
+                onClick={() => signIn(id, { callbackUrl: "/" })}
+                className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
+              >
+                Sign in with {name}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+}
